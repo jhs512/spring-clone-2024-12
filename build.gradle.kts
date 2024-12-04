1901864c391633890d5687669cc9be96789948aa
jhs512 <jangka512@gmail.com>
2024-12-05 00:46:13 +0900

    6강 : 롬복, 잭슨, 리플렉션 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,18 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.36")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    implementation("org.reflections:reflections:0.10.2")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
 }
 
 tasks.test {
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # 작업내역
 
+## 6강 : 롬복, 잭슨, 리플렉션 의존성 추가
+
 ## 5강 : test(green) : ApplicationContext init
 
 ## 4강 : test(red) : ApplicationContext init
