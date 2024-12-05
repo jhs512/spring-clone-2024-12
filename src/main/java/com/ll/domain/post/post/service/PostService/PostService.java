7abf97ace6c49e3c66d763a9fbf9138f8b077e65
jhs512 <jangka512@gmail.com>
2024-12-06 00:54:50 +0900

    18강 : test(red) : PostService 가 PostRepository 에 의존하도록 수정

--- src/main/java/com/ll/domain/post/post/service/PostService/PostService.java
+++ src/main/java/com/ll/domain/post/post/service/PostService/PostService.java
@@ -1,7 +1,11 @@
 package com.ll.domain.post.post.service.PostService;
 
+import com.ll.domain.post.post.repository.PostRepository;
 import com.ll.framework.ioc.annotations.Service;
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
 }
